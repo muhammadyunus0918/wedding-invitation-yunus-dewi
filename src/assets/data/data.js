5470239289f9cc47ee5e71a399676183057ba926
Yunus <muhammadyunus0918@gmail.com>
2025-10-04 13:21:05 +0800

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,140 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Muhammad Yunus, S.T.',
+            child: 'Putra pertama',
+            father: 'Ambo Tuo',
+            mother: 'Muliati',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Dewi Fatimah Ihsary, S.K.M.',
+            child: 'Putri pertama',
+            father: 'Ihsan, S.Pd.',
+            mother: 'Dra. Ernashari Akil, M.Si.',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'Desember',
+            date: '05',
+            day: 'Jum'at',
+            hours: {
+                start: '10.00',
+                finish: 'Selesai'
+            }
+        },
+		address: 'Masjid Fastabiqul Khaerat, Mattirotappareng, Kec. Tempe, Kab. Wajo, Prov. Sulsel (90911)'
+        reception 1 : {
+            year: '2025',
+            month: 'Desember',
+            date: '07',
+            day: 'Ahad',
+            hours: {
+                start: '10.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Jln. Mattuppuang No. 079, Pajalele, Kec. Tanasitolo, Kab. Wajo, Prov. Sulsel (90951)'
+		reception 2 : {
+            year: '2025',
+            month: 'Desember',
+            date: '07',
+            day: 'Ahad',
+            hours: {
+                start: '13.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Sallo Hotel Sengkang, Jln. H. Bahe, Tempe, Kec. Tempe, Kab. Wajo, Prov. Sulsel (90911)'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/oSVLRMYC79GzuA4f9',
+        "Akad Nikah" map: 'https://maps.app.goo.gl/5cqiyNUJigDj2yJS6,
+		"Resepsi Mempelai Pria" map: 'https://maps.app.goo.gl/JcVr5CCRmqLeqRRt9,
+		"Resepsi Mempelai Wanita" map: 'https://maps.app.goo.gl/VYHKa2LLEnyCmSdJA,
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Muhammad Yunus',
+            icon: './src/assets/images/bca.png',
+            rekening: '1620006922284'
+        },
+        {
+            id: 2,
+            name: 'Dewi Fatimah Ihsary',
+            icon: './src/assets/images/bri.png',
+            rekening: '7211630013'
+        },
+    ],
+
+    audio: './src/assets/audio/Terbuang Dalam Waktu - Barasuara (Peaceful Piano).mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
